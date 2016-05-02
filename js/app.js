2fbf888fa4645b58962b7a7a1ee23fed2ebba1f9
cfergus <christan.fergus@doortodoororganics.com>
2016-05-02 16:28:14 -0600

    major features added but still missing a few npm modules will not load over this connection to pushing to do at home

--- js/app.js
+++ js/app.js
@@ -1,35 +1,54 @@
-(function(){
-	var app = angular.module('resourcesSite', [ ]); 
+(function() {
+    var app = angular.module('resourcesSite', []);
 
-	app.controller('ResourceController', function(){
-		this.item = resourceItem;
-	});
+    app.controller('ResourceController', function($scope) {
+        // TODO: hook up filtering
+        $scope.resourceType = [{
+            id: 1,
+            name: 'Book'
+        }, {
+            id: 2,
+            name: 'Website'
+        }, {
+            id: 3,
+            name: 'Podcast'
+        }];
 
-	var resourceItem = [
-		{
-			name: 	'JavaScript for Web Developers',
-			type: 	'Book'
-		},
-		{
-			name: 	'Muz.li',
-			type: 	'Website'
-		},
-		{
-			name: 	'The Web Ahead',
-			type: 	'Podcast'
-		}
-	];
+        $scope.items = [{
+            name: 'JavaScript for Web Developers',
+            // will be replaced by updated filter array
+            type: 'Book',
+            url: 'http://christanfergus.com',
+            date: new Date()
+        }, {
+            name: 'Muz.li',
+            type: 'Website',
+            url: 'http://christanfergus.com',
+            date: new Date()
+        }, {
+            name: 'The Web Ahead',
+            type: 'Podcast',
+            url: 'http://christanfergus.com',
+            date: new Date()
+        }];
 
 
-	app.controller('addController', function($scope) {
-		$scope.addResource = function() {
-	        resourceItem.push(
-	        	{
-	        		name: 	$scope.titleInput
-	        	}
-	        );
-	        $scope.titleInput = "";
-    	};
-	});
+        $scope.add = function() {
+            $scope.items.push({
+                name: $scope.titleInput,
+                type: $scope.typeInput,
+                url: $scope.urlInput,
+                date: new Date()
+            });
+            // I know there must be a better way than this to clear the fields!
+            $scope.titleInput = '';
+            $scope.typeInput = '';
+            $scope.urlInput = '';
+        };
+
+        $scope.remove = function(index) {
+            $scope.items.splice(index, 1);
+        };
+    });
 
 })();
